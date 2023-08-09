67a0bb674fc49a2670b9bb8d4b81722423db5c21
Tahir1072a <tahirifdn@gmail.com>
2023-08-09 19:10:40 +0300

    Biraz viewde iyileştirmeler yapıldı!

---
+++ Constraints/CustomConstraints.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace WebApplication1.Constraints
+{
+    public class CustomConstraints : IRouteConstraint
+    {
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            return true;
+        }
+    }
+}
