cc39d5881bc5af00b2a821ad893b2fa434df9c63
Tahir1072a <tahirifdn@gmail.com>
2023-08-09 13:25:47 +0300

    ViewComponent Eklendi

---
+++ ViewComponents/HakkımdaViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication1.ViewComponents
+{
+    public class HakkımdaViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            List<string> bilgilerim = new List<string>();
+            bilgilerim.Add("Tahiri");
+            bilgilerim.Add("Web Developer");
+            bilgilerim.Add("tahirifdn@gmail.com");
+            return View(bilgilerim);
+        }
+    }
+}
